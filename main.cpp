f186a181bb395e3488cdd9d5c6809893a5743ee9
madcode2503 <133625813+madcode2503@users.noreply.github.com>
2023-09-20 22:42:23 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,64 +15,69 @@ int main()
     cout << "No" << setw(9) << "Time" << setw(14) << "Flight " << setw(16) << "Destination" << setw(23) << "Air manufacturer " << setw(25) << "Destination country";
     cout << endl;
     f.PrintList(f);
-    cout << "Choose 1 of 3 choices below\n";
-    cout<<"1.Add new flights to the list\n"
-        <<"2.Remove a flight with the flight number you input: \n"
-        <<"3.None of these above: \n"
-        <<"Your choice: ";
-    cin >> choices;
-    switch (choices)
+    do
     {
-    case 1:
-        do
-        {
-            cout << "Do you wanna add another flight?(Y/N) \n";
-            cin.ignore();
-            getline(cin, x);
-            if (x == "y")
-            {
-                f.add(f);
-            }
-            else if (x == "n"||x=="N")
-            {
-                f.Sort(f);
-                cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Departures<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n";
-                cout << endl;
-                cout << "No" << setw(9) << "Time" << setw(14) << "Flight " << setw(16) << "Destination" << setw(23) << "Air manufacturer " << setw(25) << "Destination country";
-                cout << endl;
-                f.PrintList(f);
-            }
+        cout << "Choose 1 of 3 choices below\n";
+        cout << "1.Add new flights to the list\n"
+             << "2.Remove a flight with the flight number you input: \n"
+             << "3.Exit! \n"
+             << "Your choice: ";
+        cin >> choices;
 
-        } while (x == "y"||x=="Y");
-        break;
-    case 2:
-        do
+        system("cls");
+        switch (choices)
         {
-            cout << "Do u wanna delete any flights?(Y?N)";
-            cin.ignore();
-            getline(cin, h);
-            if (h == "y")
+        case 1:
+            do
             {
-                cout << "Input the Flight number in the list: ";
-                getline(cin, fnum);
-                f.FaD(f, fnum);
-            }
-            if (h == "n"||h=="N")
+                cout << "Do you wanna add another flight?(Y/N) \n";
+                cin >> x;
+                if (x == "y")
+                {
+                    f.add(f);
+                }
+                else if (x == "n" || x == "N")
+                {
+                    f.Sort(f);
+                    cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Departures<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n";
+                    cout << endl;
+                    cout << "No" << setw(9) << "Time" << setw(14) << "Flight " << setw(16) << "Destination" << setw(23) << "Air manufacturer " << setw(25) << "Destination country";
+                    cout << endl;
+                    f.PrintList(f);
+                }
+
+            } while (x == "y" || x == "Y");
+            break;
+        case 2:
+            do
             {
-                f.Sort(f);
-                cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Departures<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n";
-                cout << endl;
-                cout << "No" << setw(9) << "Time" << setw(14) << "Flight " << setw(16) << "Destination" << setw(23) << "Air manufacturer " << setw(25) << "Destination country";
-                cout << endl;
-                f.PrintList(f);
-            }
-        } while (h == "y"||h=="Y");
-        break;
-    case 3:
-        break;
-    default:
-        cout << "Invalid choice!" << endl;
-    }
+                cout << "Do u wanna delete any flights?(Y?N)";
+                cin >> h;
+                if (h == "y")
+                {
+                    cout << "Input the Flight number in the list: ";
+                    cin.ignore();
+                    getline(cin, fnum);
+                    f.FaD(f, fnum);
+                }
+                if (h == "n" || h == "N")
+                {
+                    f.Sort(f);
+                    cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Departures<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n";
+                    cout << endl;
+                    cout << "No" << setw(9) << "Time" << setw(14) << "Flight " << setw(16) << "Destination" << setw(23) << "Air manufacturer " << setw(25) << "Destination country";
+                    cout << endl;
+                    f.PrintList(f);
+                }
+            } while (h == "y" || h == "Y");
+            break;
+        case 3:
+            cout << "Thank you !!!\n";
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+        }
+    } while (choices == 1 || choices == 2);
 
     system("pause");
     return 0;
